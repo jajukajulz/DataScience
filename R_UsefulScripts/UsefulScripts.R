9a51840081df7102e73970abb7ba245bcc2a5dbc
juliank <julian.kanjere@jskdezign.com>
2019-08-14 19:09:24 +0200

    Add snippet for setting working directory to currently open script

--- R_UsefulScripts/UsefulScripts.R
+++ R_UsefulScripts/UsefulScripts.R
@@ -5,3 +5,9 @@
 #remove all the objects from the workspace
 rm(list=ls())
 
+#set working directory to path of currently open script
+library(rstudioapi)
+current_path = rstudioapi::getActiveDocumentContext()$path  #Get path of current open file
+setwd(dirname(current_path ))#setworking directory
+print( getwd() )
+
