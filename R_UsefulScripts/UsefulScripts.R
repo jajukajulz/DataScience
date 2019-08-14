950acbf05c212171a8676cf62a8f4efead4e1169
juliank <julian.kanjere@jskdezign.com>
2019-08-14 19:46:01 +0200

    Add snippet for removing rows with NA or NaN values

--- R_UsefulScripts/UsefulScripts.R
+++ R_UsefulScripts/UsefulScripts.R
@@ -2,16 +2,16 @@
 #author: "Julian Kanjere"
 #date: "August 2019"
 
-#remove all the objects from the workspace
+#Remove all the objects from the workspace
 rm(list=ls())
 
-#set working directory to path of currently open script
+#Set working directory to path of currently open script
 library(rstudioapi)
 current_path = rstudioapi::getActiveDocumentContext()$path  #Get path of current open file
 setwd(dirname(current_path ))#setworking directory
 print( getwd() )
 
-#set seed so that sampling can be reproduced in future
+#Set seed so that sampling can be reproduced in future
 set.seed(10)
 
 #80%/20% Train Test Split
@@ -19,49 +19,53 @@ train = sample(1:nrow(mtcars), nrow(mtcars)*.80)
 mtcars.test = mtcars[-train,]
 mtcars.train = mtcars[train,]
 
-#view first 6 rows and all columns (features) of dataset
+#View first 6 rows and all columns (features) of dataset
 head(mtcars)
 
-#return number of columns/features in dataset
+#Return number of columns/features in dataset
 length(mtcars) #expect this to be 11
 
-#return column names from dataset
+#Return column names from dataset
 names(mtcars)
 #colnames(mtcars)
 
-#check number of observations/cases/rows and features/variables/columns in dataset
+#Check number of observations/cases/rows and features/variables/columns in dataset
 dim(mtcars) #this should return 32 observations and 11 features
 
-#check dataset column types
+#Check dataset column types
 sapply(mtcars, class)
 
-#check dataset column types and get more info
+#Check dataset column types and get more info
 str(mtcars) #Returns column types and more info such as the levels of your factors and the first few values of each variable)
 
-#convert a dataset column to factor/category variable (i.e. so R does not confuse the feature with numerical variables)
+#Convert a dataset column to factor/category variable (i.e. so R does not confuse the feature with numerical variables)
 mtcars[,9] <- factor(mtcars[,9]) #convert column 9 which is transmission type manual or automatic
 mtcars$am <- factor(mtcars$am) #alternative to above
 
-#for loop
+#For loop
 for(i in 1:length(mtcars)){
     cat("Iteration number = ", i)
     #If you want the entire output returned as a string use sprintf or paste
     #mystring <- sprintf("Iteration number = ", i)
 }
 
-#read csv
+#Read csv
 USBabyNames <- read.csv("USBabyNames.csv")
 
 #Drop ID variable if in dataset
 USBabyNames <- USBabyNames[,-1] #Drop ID variable which is first column
 
-#check for missing values
+#Check for missing values
 colSums(is.na(USBabyNames))
 
 #Retain only complete cases
 USBabyNames <- USBabyNames[complete.cases(USBabyNames),]
 
-#update missing values for Count using median values
+
+#Remove rows with NA or NaN in Count column from the dataset (Generally not a good approach although useful in some cases)
+USBabyNames <- USBabyNames[!is.na(USBabyNames$Count),]
+
+#Update missing values for Count using median values
 summary(USBabyNames)
 Count.median <- median(USBabyNames$Count, na.rm=TRUE) #calculate median excluding missing values
 USBabyNames$Count[is.na(USBabyNames$Count)] <- Count.median
