b47ede9325cc58b7554c96d1c4e19be570cc17ca
juliank <julian.kanjere@jskdezign.com>
2019-09-12 19:38:07 +0200

    Machine Learning pipeline template

---
+++ R_UsefulScripts/ML_Pipeline_Template.R
@@ -0,0 +1,17 @@
+#title: "Machine Learning pipeline template"
+#author: "Julian Kanjere"
+#date: "September 2019"
+
+# 1. Workspace Setup
+# 2. Data Import
+# 3. Data Cleansing
+# 4. Exploratory Data Analysis
+# 5. Feature Engineering and Selection
+# 6. Baseline Model Building (build multiple models with different algorithms)
+# 7. Model Evaluation (evaluate the different models and select top 3)
+# 8. Hyperparameter Tuning (to improve on the best 3 baseline models)
+# 9. Model Evaluation (evaluate performance of best 3 baseline models after tuning)
+# 10. Model Interpretation
+# 11. Write Up (Report on problem, modelling activities, solution and interpretation)
+# 12. Presentation (Summary of write up for management)
+# 13. Production Deployment
