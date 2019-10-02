697dbbf94f7ec1c9682cfd21db73833912eaf454
juliank <julian.kanjere@jskdezign.com>
2019-10-02 12:59:47 +0200

    Add Cluster Analysis

---
+++ R_ClusterAnalysis/ClusterAnalysis.R
@@ -0,0 +1,21 @@
+#title: "Cluster Analysis Tutorial (Unsupervised Learning)"
+#author: "Julian Kanjere"
+#date: "October 2019"
+
+#Cluster Analysis.
+#Grouping of items based on similarity/dissimilarity given by a distance measure. Requires quantitative variables
+
+#1. kMeans Clustering
+# ABC
+
+#2.	Hierarchical Clustering
+# ABC
+
+#3. Density Based (DBSCAN)
+# ABC
+
+
+####################START kMeans USING PSL DATASET##########################################################
+#The PSL dataset was scraped from South African Premier Soccer League website.
+####################END kMeans USING PSL DATASET##########################################################
+
