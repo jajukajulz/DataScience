863889e7726453e424de6ac3777eed638da943af
juliank <julian.kanjere@jskdezign.com>
2019-10-02 14:02:26 +0200

    Add notes on determining optimal number of clusters as well as validating cluster results

--- R_ClusterAnalysis/ClusterAnalysis.R
+++ R_ClusterAnalysis/ClusterAnalysis.R
@@ -3,7 +3,8 @@
 #date: "October 2019"
 
 #Cluster Analysis.
-# Grouping of items based on similarity/dissimilarity given by a distance measure.
+# Grouping of items based on similarity/dissimilarity given by a distance measure. Clustering is an unsupervised machine learning method for partitioning dataset into a set of groups or clusters. A big issue is that clustering methods will return clusters even if the data does not contain any clusters.
+# Therefore, it’s necessary i) to assess clustering tendency before the analysis and ii) to validate the quality of the result after clustering.
 
 #1. kMeans Clustering
 # ABC
@@ -18,14 +19,46 @@
 
 # hc_average <- hclust(distance, method = "average") gives a cluster dendrogram with average linkage
 
-# compare cluster formation between complete linkage and average linkage
+# compare cluster formation between complete linkage and average linkage (External clustering validation)
 # cluster_members_complete <- cutree(hc_complete, k = 3)
 # cluster_members_average <- cutree(hc_average, k = 3)
 # table(cluster_members_complete, cluster_members_average)
 
+# compare cluster means for the different variables to get an idea of which variables influence cluster membership
+# i.e. if cluster means for a variable are not too different then that variable does not strongly influence cluster membership
+# aggregate(dataset, list(cluster_members_complete), mean)
+
+
 #3. Density Based (DBSCAN)
 # ABC
 
+# Determine the optimal number of clusters
+# 1. Scree plot elbow
+# 2. Many indices - use NbClust() and choose best number of clusters according to the majority rule
+#  Compute the number of clusters
+# library(NbClust)
+# nb <- NbClust(scaled_dataset, distance = "euclidean", min.nc = 2, max.nc = 10, method = "complete", index ="all")
+
+# Visualize the result
+# library(factoextra)
+# fviz_nbclust(nb) + theme_minimal()
+
+# Internal clustering validation measures
+# The goal of clustering algorithms is to split the dataset into clusters of objects, such that objects in the same cluster are similar as much as possible and objects in different clusters are highly distinct.
+# i.e. The average distance within cluster should be as small as possible; and the average distance between clusters should be as large as possible.
+
+# silhouette plot
+# Silhouette refers to a method of interpretation and validation of consistency within clusters of data. The technique provides a succinct graphical representation of how well each object has been classified.
+# Silhouette analysis measures how well an observation is clustered and it estimates the average distance between clusters. The silhouette plot displays a measure of how close each point in one cluster is to points in the neighboring clusters.
+# If many points have a low or negative value, then the clustering configuration may have too many or too few clusters.
+# If only a handful have negative values, then they are possibly outliers
+# silhouette width is also an estimate of the average distance between clusters. Its value is comprised between 1 and -1 with a value of 1 indicating a very good cluster.
+# Silhouette width (Si) can be interpreted as follows: Observations with a large Si (almost 1) are very well clustered, A small Si (around 0) means that the observation lies between two clusters, Observations with a negative Si are probably placed in the wrong cluster.
+
+# External clustering validation
+# Objective is to compare the identified clusters (by k-means, hierarchical clustering, PAM etc) to a known reference (truth).
+# To compare two cluster solutions, use cluster.stats() function e.g. results.stat <- cluster.stats(d, solution1$cluster, solution2$cluster)
+# Alternatively cross tabulation between 2 cluster techniques or 1 cluster technique and truth e.g. table(iris$Species, km.res$cluster)
 
 ####################START kMeans USING PSL DATASET##########################################################
 #The PSL dataset was scraped from South African Premier Soccer League website.
