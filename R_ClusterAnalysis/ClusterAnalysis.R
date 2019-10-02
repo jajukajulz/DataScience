4efb7e49e642b8aa4ffc091fa41483fed45b8143
juliank <julian.kanjere@jskdezign.com>
2019-10-02 13:29:27 +0200

    Add notes for hierarchical clustering with complete linkage

--- R_ClusterAnalysis/ClusterAnalysis.R
+++ R_ClusterAnalysis/ClusterAnalysis.R
@@ -3,13 +3,18 @@
 #date: "October 2019"
 
 #Cluster Analysis.
-#Grouping of items based on similarity/dissimilarity given by a distance measure. Requires quantitative variables
+# Grouping of items based on similarity/dissimilarity given by a distance measure.
 
 #1. kMeans Clustering
 # ABC
 
 #2.	Hierarchical Clustering
-# ABC
+# hc_complete <- hclust(distance) gives a cluster dendrogram with complete linkage
+# to obtain cluster dendrogram you use plot() command i.e. plot(hc_complete)
+# to add labels to cluster dendrogram you add labels to plot() command i.e. plot(hc_complete, labels=row_labels)
+# to make the labels aligned, use hang attribute i.e. plot(hc_complete, labels=row_labels, hang = -1)
+# initially each observation is treated as a single cluster then based on distance you join clusters, this process is repeated until all observations are linked to each other
+
 
 #3. Density Based (DBSCAN)
 # ABC
@@ -17,5 +22,8 @@
 
 ####################START kMeans USING PSL DATASET##########################################################
 #The PSL dataset was scraped from South African Premier Soccer League website.
+# Requires quantitative variables.
+# Normalize variables (mean becomes zero and std dev becomes 1) which levels playing field so that there are no variables that dominate the analysis.
+# Calculate Euclidean distance using dist()
 ####################END kMeans USING PSL DATASET##########################################################
 
