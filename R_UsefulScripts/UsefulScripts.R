95e5622552fb28c50a1708ef0e9a5a8f697f737b
juliank <julian.kanjere@jskdezign.com>
2019-10-03 21:16:33 +0200

    Ad snippet for Euclidean distance implementation below

--- R_UsefulScripts/UsefulScripts.R
+++ R_UsefulScripts/UsefulScripts.R
@@ -179,3 +179,20 @@ plot(X,Y3, pch = 16)
 n <- 10
 s <- sample(1:100, n)
 s
+
+#Euclidean distance
+#Euclidean distance or Euclidean metric is the "ordinary" straight-line distance between two points in Euclidean space.
+#FYI Euclidean distance for 2 dimensions is similar to Pythagoras' theorem - It states that the area of the square whose side is the hypotenuse (side opposite the right angle) is equal to the sum of the areas of the squares on the other two sides.
+#Pythagoras' theorem can be written as an equation relating the lengths of the sides a, b and c: a^2 + b^2 = c^2 where c represents the length of the hypotenuse and a and b the lengths of the triangle's other two sides.
+#Euclidean distance implementation below
+sum_i_to_n <- 0
+n <- 2 #number of dimensions
+q <- c(1,3) #i.e. 2 dimensions, coordinate for q is x, y
+p <- c(2,5) #i.e. 2 dimensions, coordinate for q is x, y
+for (i in 1:n){
+  # i is the current dimension we are looking at
+  sum_i = (q[i] - p[i])^2
+  sum_i_to_n = sum_i_to_n + sum_i
+}
+euclidean_dist = sqrt(sum_i_to_n)
+euclidean_dist
